3e780d6a4ae0cb295361dcb2504636b80c831719
johnirvenfebrada84-bit <johnirvenfebrada84@gmail.com>
2026-03-13 05:34:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -15,5 +15,4 @@ def get_student():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run()
